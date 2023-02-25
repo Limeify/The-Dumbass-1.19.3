84c5b724dae062febf26ff8090c170eee5a050b2
Limeify <limeify@duck.com>
2023-02-25 00:20:27 -0500

    programmed Thursday Morning Piglin, no AI yet but his animations are working

--- src/main/java/net/limeify/thedumbass/TheDumbass.java
+++ src/main/java/net/limeify/thedumbass/TheDumbass.java
@@ -1,8 +1,12 @@
 package net.limeify.thedumbass;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.limeify.thedumbass.entity.ModEntities;
+import net.limeify.thedumbass.entity.custom.ThursdayMorningPiglin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import software.bernie.geckolib.GeckoLib;
 
 public class TheDumbass implements ModInitializer {
 	public static final String MOD_ID = "thedumbass";
@@ -10,5 +14,7 @@ public class TheDumbass implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		GeckoLib.initialize();
+		FabricDefaultAttributeRegistry.register(ModEntities.THURSDAY_MORNING_PIGLIN, ThursdayMorningPiglin.createPiglinAttributes());
 	}
 }
--- src/main/java/net/limeify/thedumbass/TheDumbassClient.java
+++ src/main/java/net/limeify/thedumbass/TheDumbassClient.java
@@ -1,11 +1,14 @@
 package net.limeify.thedumbass;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.limeify.thedumbass.entity.ModEntities;
+import net.limeify.thedumbass.entity.client.ThursdayMorningPiglinRenderer;
 
 public class TheDumbassClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-
+        EntityRendererRegistry.register(ModEntities.THURSDAY_MORNING_PIGLIN, ThursdayMorningPiglinRenderer::new);
     }
 }
